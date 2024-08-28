89a9f95cb7121ee2442c7e311feb425be039de5b
kajonsak002 <651463002@crru.ac.th>
2024-08-28 15:22:17 +0700

    next-dashborad-learning

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="mb-4 text-xl md:text-2xl">Customers</h1>
+    </>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import Card from "@/app/ui/dashboard/cards";
+export default function Page() {
+  return (
+    <>
+      <h1 className="mb-4 text-xl md:text-2xl">Dashboard</h1>
+    </>
+  );
+}
